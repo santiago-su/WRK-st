3a9ec2b6573e7fb1e9b2eb0b58d807d0d5041e25
Santiago Suárez <santiago.su.mo@gmail.com>
2014-09-02 02:16:39 -0500

    Saldo

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,20 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :transactions
+
+  def balance
+    total = 0
+    x = 0
+    y = 0
+    ingr = Transaction.where(type_of_transaction: "1")
+    egre = Transaction.where(type_of_transaction: "2")
+    ingr.each { |a| x += a.amount }
+    egre.each { |b| y += b.amount }
+    total = x - y
+  end
+  
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,18 +1,6 @@
 class Transaction < ActiveRecord::Base
   validates_presence_of :amount, :type_of_transaction, :concept, :date
 
-  def ingreso(transaction)
-    if transaction.type_of_transaction == "1"
-      "x"
-    end
-  end
-
-  def egreso(transaction)
-    if transaction.type_of_transaction == "2"
-      "x"
-    end
-  end
-
   def balance
     total = 0
     x = 0
